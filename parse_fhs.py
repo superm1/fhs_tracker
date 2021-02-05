52000112500288782db462e0460d4a82277965a1
Mario Limonciello <mario.limonciello@dell.com>
2021-02-05 09:50:57 -0600

    drop unneeded imports

--- parse_fhs.py
+++ parse_fhs.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from secrets import uri
 import paho.mqtt.client as mqttClient
 import time
